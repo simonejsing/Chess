a93239df2e9b9de62d06786943564119d59f9771
Simon Kristiansen Ejsing <sejsing@gmail.com>
2020-08-10 22:20:27 -0700

    Migrate to MonoGame 3.8

---
+++ Chess2/Game1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Chess2
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D pixel;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            pixel = new Texture2D(_spriteBatch.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            pixel.SetData(new[] { Color.White });
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+
+            DrawChessBoard(20, 20);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        private void DrawChessBoard(int x, int y)
+        {
+            // Draw 8x8 chess board
+            for (int j = 0; j < 8; j++)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    var color_index = (i + j % 2) % 2;
+                    Color c = color_index == 0 ? Color.White : Color.Black;
+                    int x2 = i * 64;
+                    int y2 = j * 64;
+                    _spriteBatch.Draw(pixel, new Rectangle(x + x2, y + y2, 64, 64), c);
+                }
+            }
+        }
+    }
+}
---
+++ Chess2/SpriteLoader.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess2
+{
+    public class Sprite
+    {
+        public Texture2D Texture { get; private set; }
+
+        public Sprite(Texture2D texture)
+        {
+            this.Texture = texture;
+        }
+    }
+
+    public class SpriteLoader
+    {
+        private ContentManager manager;
+
+        public SpriteLoader(ContentManager manager)
+        {
+            this.manager = manager;
+        }
+
+        public Sprite Load(string filename)
+        {
+            return new Sprite(this.manager.Load<Texture2D>(@"sprites\" + filename));
+        }
+    }
+}
